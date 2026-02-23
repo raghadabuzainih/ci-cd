780835c0c941205bedea0bcca6444f2c35a5f50f
raghadabuzainih <raghad.abuzainih@gmail.com>
2026-02-23 23:33:53 +0200

    update eslint.config file

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,16 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    ignores: ["dist/**", "node_modules/**"]
+  },
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { 
+      globals: globals.browser 
+    }
+  },
   tseslint.configs.recommended,
 ]);
