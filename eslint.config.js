4b1134b64b1319e7a1a7a419ac973268e272854b
raghadabuzainih <raghad.abuzainih@gmail.com>
2026-02-24 10:56:40 +0200

    add eslint-plugin-security & make this check auto in CI

--- eslint.config.js
+++ eslint.config.js
@@ -2,18 +2,17 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import pluginSecurity from 'eslint-plugin-security'
 
 export default defineConfig([
-  {
-    ignores: ["dist/**", "node_modules/**"]
-  },
   { 
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], 
     plugins: { js }, 
     extends: ["js/recommended"], 
     languageOptions: { 
-      globals: globals.browser 
+      globals: globals.node 
     }
   },
   tseslint.configs.recommended,
+  pluginSecurity.configs.recommended
 ]);
