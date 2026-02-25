fa346ce417045024fe18bfd4a7f75150ffa20b56
raghadabuzainih <raghad.abuzainih@gmail.com>
2026-02-25 21:43:19 +0200

    fix migrations error'

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "drizzle-kit";
 
+import { config } from "./src/config";
+
 export default defineConfig({
   out: "./src/db/migrations",
   schema: "./src/db/schema.ts",
   dialect: "turso",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: config.db.url || "",
   },
 });
